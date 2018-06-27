80f307c056019e5315d177a0ecfee70cc6ab3f93
josaphat-mayubandele <40607254+josaphat-mayubandele@users.noreply.github.com>
2018-06-27 16:03:58 -0400

    add database with mongoose
    add logger with morgan
    add cookie parser

---
+++ back-end/database/db.config.ts
@@ -0,0 +1,24 @@
+import { connect } from 'mongoose';
+/*
+*   Database connection to mongoDB
+*/
+
+const db = {
+  ADDRESS: 'localhost',
+  PORT: 27017,
+  NAME: 'sprint-ng'
+};
+
+export class DBConfig {
+  static connectMongoDB() {
+    connect(
+      `mongodb://${db.ADDRESS}/${db.NAME}`,
+      err => {
+        if (err) {
+          console.log('Faied to connect to DB');
+        }
+        console.log('Successfully connected to MongoDB');
+      }
+    );
+  }
+}
--- server.ts
+++ server.ts
@@ -1,18 +1,41 @@
+import * as bodyParser from 'body-parser';
 import * as express from 'express';
+import * as logger from 'morgan';
+import * as cookieParser from 'cookie-parser';
 
 // Import WelcomeController from controllers entry point
 import { WelcomeController } from './back-end/controllers';
+// Import database configuration
+import { DBConfig } from './back-end/database/db.config';
 
 // Create a new express application instance
 const app: express.Application = express();
 // The port the express app will listen on
 const port = process.env.PORT || 3000;
-
+//  body parsing middleware
+app.use(bodyParser.json());
 // Mount the WelcomeController at the /welcome route
 app.use('/', WelcomeController);
 
+// use logger middlware
+app.use(logger('dev'));
+
+// use json form parser middlware
+app.use(bodyParser.json());
+
+// use query string parser middlware
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+// use cookie parser middleware
+app.use(cookieParser('SECRET_GOES_HERE'));
+
 // Serve the application at the given port
 app.listen(port, () => {
-    // Success callback
-    console.log(`Listening at http://localhost:${port}/`);
+  // Success callback
+  DBConfig.connectMongoDB();
+  console.log(`Listening at http://localhost:${port}/`);
 });
