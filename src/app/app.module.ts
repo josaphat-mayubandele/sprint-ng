780ce51f7ec9330a7e5f290298f488a0b0e66d02
josaphat-mayubandele <40607254+josaphat-mayubandele@users.noreply.github.com>
2018-06-28 13:03:39 -0400

    ajout routage de la page welcome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RoutingModule } from './app.routing';
+import { HeaderComponent } from './shared/header/header.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, HeaderComponent, WelcomeComponent],
+  imports: [BrowserModule, RoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
