d01927d3514e694fd806aa1d9d76eb6c367c7e65
josaphat-mayubandele <40607254+josaphat-mayubandele@users.noreply.github.com>
2018-06-28 13:05:59 -0400

    creaiondu composant d'authentification

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { RoutingModule } from './app.routing';
 import { HeaderComponent } from './shared/header/header.component';
+import { AuthComponent } from './auth/auth.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, WelcomeComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    AuthComponent,
+    WelcomeComponent
+  ],
   imports: [BrowserModule, RoutingModule],
   providers: [],
   bootstrap: [AppComponent]
