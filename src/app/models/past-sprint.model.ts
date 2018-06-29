ab7e5a2be1e50913e3894906c2b21bb52ca4c7ff
josaphat-mayubandele <40607254+josaphat-mayubandele@users.noreply.github.com>
2018-06-29 11:00:41 -0400

    :sparkles: **Data modeling** front end
    :sparkles: **Data modeling**
    - [x] SprintTemplate
          o name
          o duration
          o status
    - [x] PastSprint
    o name
    o duration
    o status
    o progress
    o description
    o notify
    o user
    o createdAt
    o startedAt
    o finishedAt

---
+++ src/app/models/past-sprint.model.ts
@@ -0,0 +1,12 @@
+export class PastSprint {
+  name: string;
+  duratin: string;
+  status: string;
+  prgress: string;
+  descriptin: string;
+  ntify: string;
+  user: string;
+  createdAt: string;
+  startedAt: string;
+  finishedAt: string;
+}
---
+++ src/app/models/sprint-template.model.ts
@@ -0,0 +1,5 @@
+export class SprintTemplate {
+  name: string;
+  duration: string;
+  status: string;
+}
