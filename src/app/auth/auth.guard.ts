8908ee1037f16bda8f9fd6956c6c1ce34b360db5
josaphat-mayubandele <40607254+josaphat-mayubandele@users.noreply.github.com>
2018-06-29 12:16:51 -0400

    A login and logout system
    :sparkles: **A login and logout system**
    - [x] creation du module d'authentification
    - [x] cree compte Auth0
    - [x] mettre en place Auth0
    - [x] mettre le lien de la page apres login et logout
    - [ ] An indication of the user that is logged in, in all protected screens

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,14 +1,29 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import {
+  Router,
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot
+} from '@angular/router';
 import { Observable } from 'rxjs';
+
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+  constructor(private router: Router, private auth: AuthService) {}
+
   canActivate(
     next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    return true;
+    state: RouterStateSnapshot
+  ): Observable<boolean> | Promise<boolean> | boolean {
+    if (this.auth.isAuthenticated()) {
+      return true;
+    } else {
+      this.auth.login();
+      return false;
+    }
   }
 }
