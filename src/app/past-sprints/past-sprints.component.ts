4daa02293e6e74b86f62e068befa84d2cffb02b3
josaphat-mayubandele <40607254+josaphat-mayubandele@users.noreply.github.com>
2018-07-03 16:46:10 -0400

    :sparkles:  recuperation de donnee dans db
    :sparkles: **past sprint**
    - [x] create module
    - [x] routage du module
    - [x] mise en module
    - [x] create tab content:
    Length
    Status
    Date
    Start	Finish
    Description
    - [ ] sorting
    - [ ] filtering
    - [ ] pagination
    - [x] recuperation de donnee dans db
    - [ ] Offer the option of deleting all stored data concerning the user

--- src/app/past-sprints/past-sprints.component.ts
+++ src/app/past-sprints/past-sprints.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+import { PastSprintModel } from '../models/past-sprint.model';
+import { PastSprintsService } from './past-sprints.service';
+
 @Component({
   selector: 'app-past-sprints',
   templateUrl: './past-sprints.component.html',
   styleUrls: ['./past-sprints.component.css']
 })
 export class PastSprintsComponent implements OnInit {
+  currentpastsprint: PastSprintModel;
 
-  constructor() { }
+  canDeleted: boolean;
+  havePagination: boolean;
 
-  ngOnInit() {
+  constructor(private sprintService: PastSprintsService) {
+    this.getSprints();
   }
 
+  ngOnInit() {}
+
+  // recevoir les sprint de l'utilisateur current
+  getSprints() {
+    this.sprintService.getSprint().subscribe(data => {
+      this.currentpastsprint = data;
+    });
+  }
 }
--- src/app/past-sprints/past-sprints.service.ts
+++ src/app/past-sprints/past-sprints.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+import { AuthService } from '../auth/auth.service';
+import { PastSprintModel } from '../models/past-sprint.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PastSprintsService {
+  profile: any;
+  private apiUrl = 'http://localhost:3000/api/getsprint';
+  constructor(private http: HttpClient, private auth: AuthService) {}
 
-  constructor() { }
+  getSprint(): Observable<PastSprintModel> {
+    // recevoir les sprint de l'utilisateur current
+    return this.http.get<PastSprintModel>(
+      `${this.apiUrl}:${this.auth.id_token}`
+    );
+  }
 }
