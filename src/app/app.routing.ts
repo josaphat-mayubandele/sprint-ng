8d4f33c435ecfff03fdc1da499e80ebe18a9ea19
josaphat-mayubandele <40607254+josaphat-mayubandele@users.noreply.github.com>
2018-07-04 18:19:24 -0400

    :sparkles: **An authentication-protected**
    :sparkles: **An authentication-protected**
    - [x] sprint creation screen
    - [x] ongoing sprint screen
    - [x] past sprints screen

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -8,6 +8,7 @@ import { AuthGuard } from './auth/auth.guard';
 import { PastSprintsComponent } from './past-sprints/past-sprints.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { NewSprintComponent } from './new-sprint/new-sprint.component';
+import { OngoingSprintComponent } from './ongoing-sprint/ongoing-sprint.component';
 
 const routes: Routes = [
   { path: '', component: WelcomeComponent },
@@ -19,6 +20,11 @@ const routes: Routes = [
   {
     path: 'new',
     component: NewSprintComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'ongoing',
+    component: OngoingSprintComponent,
     canActivate: [AuthGuard]
   },
   { path: 'callback', component: CallbackComponent },
