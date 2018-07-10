6435ce87002b100ddb64a02bd2466aeff64973a3
josaphat-mayubandele <40607254+josaphat-mayubandele@users.noreply.github.com>
2018-07-10 15:06:58 -0400

    add header for sprint service

--- src/app/past-sprints/past-sprints.service.ts
+++ src/app/past-sprints/past-sprints.service.ts
@@ -1,25 +1,33 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { AuthService } from '../auth/auth.service';
-import { PastSprintModel } from '../models/past-sprint.model';
+import { PastSprints } from '../models/past-sprint.model';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PastSprintsService {
-  private apiUrl = 'http://localhost:3000/api/getsprint';
+  private apiUrl = 'http://localhost:3000/api/v1/pastsprint';
+  private id_token;
   constructor(private http: HttpClient, private auth: AuthService) {}
 
-  getSprint(): Observable<PastSprintModel> {
+  new() {}
+  put() {}
+  get(): Observable<any> {
+    console.log(this.auth.userId());
     // recevoir les sprint de l'utilisateur current
-    return this.http.get<PastSprintModel>(
-      `${this.apiUrl}:${this.auth.id_token}`
-    );
+    return this.http.get<any>(`${this.apiUrl}/get/${this.auth.userId()}`, {
+      headers: new HttpHeaders().set(
+        'Authorization',
+        `Bearer ${localStorage.getItem('access_token')}`
+      )
+    });
   }
 
   deleteAll() {
-    return this.http.delete(`${this.apiUrl}:${this.auth.id_token}`);
+    return this.http.delete(`${this.apiUrl}/delete/${this.auth.userId()}`);
   }
 }
