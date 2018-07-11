9bdf06e503413047391061f630aeceb71fbb7771
Mayuba Ndele <josaphat.mayubandele@cgi.com>
2018-07-11 18:58:48 -0400

    new header collapse work

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
 import { AuthService } from '../../auth/auth.service';
-
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  user: string;
+  isCollapsed = true;
   constructor(public auth: AuthService) {}
 
   ngOnInit() {}
