e091f5159ba2ed5f088d9a231cff73ea2336922a
Mayuba Ndele <josaphat.mayubandele@cgi.com>
2018-07-11 17:33:43 -0400

    ongoing for child to new sprint

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -8,8 +8,7 @@ import { AuthGuard } from './auth/auth.guard';
 import { PastSprintsComponent } from './past-sprints/past-sprints.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { NewSprintComponent } from './new-sprint/new-sprint.component';
-import { OngoingSprintComponent } from './ongoing-sprint/ongoing-sprint.component';
-
+import { OngoingSprintComponent } from './new-sprint/ongoing-sprint/ongoing-sprint.component';
 const routes: Routes = [
   { path: '', component: WelcomeComponent },
   {
@@ -19,16 +18,21 @@ const routes: Routes = [
   },
   {
     path: 'new',
-    component: NewSprintComponent,
-    canActivate: [AuthGuard]
-  },
-  {
-    path: 'ongoing',
-    component: OngoingSprintComponent,
-    canActivate: [AuthGuard]
+    canActivate: [AuthGuard],
+    children: [
+      {
+        path: '',
+        component: NewSprintComponent
+      },
+      {
+        path: 'on',
+        component: OngoingSprintComponent
+      }
+    ]
   },
   { path: 'callback', component: CallbackComponent },
-  { path: '**', component: PageNotFoundComponent }
+  { path: '**', component: PageNotFoundComponent },
+  { path: 'notfound', component: PageNotFoundComponent }
 ];
 
 @NgModule({
