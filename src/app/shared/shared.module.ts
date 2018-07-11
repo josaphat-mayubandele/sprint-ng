ed9781172c9905557a29746c093669bf88ae156a
Mayuba Ndele <josaphat.mayubandele@cgi.com>
2018-07-11 00:28:46 -0400

    add footercomponent in module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FooterComponent } from '../shared/footer/footer.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   imports: [CommonModule],
