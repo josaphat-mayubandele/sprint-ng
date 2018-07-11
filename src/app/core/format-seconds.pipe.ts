e5fc2bce9c6c0c046a101c1b3513efe12678b99c
Mayuba Ndele <josaphat.mayubandele@cgi.com>
2018-07-11 17:35:12 -0400

    format second pipe rodrigo

---
+++ src/app/core/format-seconds.pipe.ts
@@ -0,0 +1,10 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'formatSeconds'
+})
+export class FormatSecondsPipe implements PipeTransform {
+  transform(value: any, args?: any): any {
+    return value > 60 ? value / 60 + ' Minutes' : value + ' Seconds';
+  }
+}
