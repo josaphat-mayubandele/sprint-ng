865d936fd8b3b18d2ccb3166db3df3bb146b523e
Mayuba Ndele <josaphat.mayubandele@cgi.com>
2018-07-11 00:33:18 -0400

    change folder to pastsprint service

--- src/app/past-sprints/past-sprints.component.ts
+++ src/app/past-sprints/past-sprints.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
-import { PastSprintModel } from '../models/past-sprint.model';
-import { PastSprintsService } from './past-sprints.service';
+import { PastSprints } from '../models/past-sprint.model';
+import { SprintsService } from '../services/sprints.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-past-sprints',
@@ -9,25 +10,29 @@ import { PastSprintsService } from './past-sprints.service';
   styleUrls: ['./past-sprints.component.css']
 })
 export class PastSprintsComponent implements OnInit {
-  currentpastsprint: PastSprintModel;
-
+  currentpastsprint: PastSprints[] = []; // [] = [];
+  pastSprints: PastSprints[];
   canDeleted: boolean;
   havePagination: boolean;
 
-  constructor(private sprintService: PastSprintsService) {
+  constructor(private sprintService: SprintsService, private router: Router) {}
+
+  ngOnInit() {
     this.getSprints();
   }
 
-  ngOnInit() {}
-
   // recevoir les sprint de l'utilisateur current
   getSprints() {
-    this.sprintService.getSprint().subscribe(data => {
-      this.currentpastsprint = data;
+    this.sprintService.get().subscribe(data => {
+      this.currentpastsprint = data.pastsprint;
+      console.log(data);
     });
   }
 
   deleteAll() {
     this.sprintService.deleteAll();
+  }
+  newpage() {
+    this.router.navigate([{ outlets: { popup: ['new'] } }]);
   }
 }
